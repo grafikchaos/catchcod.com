7f7fca815efc41e73f68bc12ef56b03722f7dae8
Josh Johnson <josh@augustash.com>
2012-09-07 13:51:03 -0500

    new content for the COD page

--- center_organizational_development.php
+++ center_organizational_development.php
@@ -1,37 +1,45 @@
 <?php
-
-//	profile.php
+/**
+ * profile.php
+ */
 
 include("headFootNavSBar.php");
-
 displayHead();
-
 ?>
 
-	<div id="mainContent">
-		<div id="content">
-			<h1>Center for Organizational Development</h1><br/>
-		
-			<p>The Center for Organizational Development (COD) is a private business with a 16-year record of outstanding success in the fields of philanthropic consulting, leadership mentoring and organizational change.</p>
-			<p>Owner and primary professional of COD Dr. Jim Storm is recognized for his expertise, understanding and ability to form professional relationships with clients which produce positive results. He gets it!</p>
-			<p>COD has been engaged in projects at the local, national and international level with a very diverse set of clients.</p>
-			<p>Work is undertaken only when the mission of the client organization is in concert with the values of COD and only when it is determined that a strong positive working relationship can be developed.</p>
-			<p>COD utilizes independent contractors in a variety of projects based on their education, experience and expertise. Selection of independent contractors when appropriate is made between COD and client organizations.</p>
-		</div>	<!-- END CONTENT -->
-	</div>	<!-- END MAIN CONTENT -->
-	
-	<div id="sidebar">
+<div id="mainContent">
+	<div id="content">
+		<h1>Center for Organizational Development</h1>
+		<p>
+			The Center For Organizational Development (COD) has had a record of outstanding success
+			in the fields of philanthropic consulting, leadership mentoring and organizational change
+			since 1995.
+		</p>
+
+		<p>
+			Owner Dr. Jim Storm is recognized for his expertise, understanding and ability to form professional
+			relationships with clients, which produce positive client-owned results.
+		</p>
+		<p>
+			COD has been successful at the local, national and international level with very diverse clients.
+		</p>
+		<p>
+			Work is undertaken only when the mission of the client is in concert with the values of COD and
+			only when it is determined that a strong positive working relationship exists.
+		</p>
+	</div>	<!-- END CONTENT -->
+</div>	<!-- END MAIN CONTENT -->
+
+<div id="sidebar">
+	<?php
+		//	display Little Red Bus image and sidebar note
+		displayBus_SB();
+
+		//	display Master of Creative Philanthropy Book and sidebar note
+		displayBook_SB();
+	?>
+</div>	<!-- END SIDEBAR -->
 <?php
-
-//	display Little Red Bus image and sidebar note
-displayBus_SB();
-
-//	display Master of Creative Philanthropy Book and sidebar note
-displayBook_SB();
-?>
-	</div>	<!-- END SIDEBAR -->
-<?php
-
-//	display footer
-displayFooter();
+	//	display footer
+	displayFooter();
 ?>
