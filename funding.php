16f5b28a4e3d9aa743c82ad39ee565cf13dfacb8
Josh Johnson <josh@augustash.com>
2012-09-06 23:36:21 -0500

    remove unnecessary <br /> tags and add leading slashes to links

--- funding.php
+++ funding.php
@@ -10,15 +10,15 @@ displayHead();
 
 	<div id="mainContent">
 		<div id="content">
-			<h1>Funding Information</h1><br/><br />
+			<h1>Funding Information</h1>
 			<ul class="bodyBullets">
-				<li><a href="funding_athwin.php">Athwin Foundation</a></li>
-				<li><a href="funding_vmb.php">VMB Fund</a></li>
-				<li><a href="funding_private_philanthropists.php">Private Philanthropists</a></li>
+				<li><a href="/funding_athwin.php">Athwin Foundation</a></li>
+				<li><a href="/funding_vmb.php">VMB Fund</a></li>
+				<li><a href="/funding_private_philanthropists.php">Private Philanthropists</a></li>
 			</ul>
 		</div>	<!-- END CONTENT -->
 	</div>	<!-- END MAIN CONTENT -->
-	
+
 	<div id="sidebar">
 <?php
 
