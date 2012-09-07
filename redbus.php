f0b9b53a7cebfb7349c8d41558cb66d969494b27
Josh Johnson <josh@augustash.com>
2012-09-06 23:34:49 -0500

    make sure side-nav links start with '/redbus?'...

--- redbus.php
+++ redbus.php
@@ -16,23 +16,23 @@ displayHead();
 	<div id="sidebar">
 		<h2>Little Red Bus Archive</h2>
 		<ul id="stories">
-			<li><a href="?story=charting">Charting the Course</a></li>
-			<li><a href="?story=driving">Who's Driving the Bus?</a></li>
-			<li><a href="?story=doubting">Intelligent Doubting</a></li>
-			<li><a href="?story=resources">Insufficient Resources</a></li>
-			<li><a href="?story=founder">The Founder</a></li>
-			<li><a href="?story=chemistry">The Chemistry</a></li>
-			<li><a href="?story=maintenance">Maintenance and Repair</a></li>
-			<li><a href="?story=listening">Listening to New Voices</a></li>
-			<li><a href="?story=checkup">The Check Up</a></li>
+			<li><a href="/redbus.php?story=charting">Charting the Course</a></li>
+			<li><a href="/redbus.php?story=driving">Who's Driving the Bus?</a></li>
+			<li><a href="/redbus.php?story=doubting">Intelligent Doubting</a></li>
+			<li><a href="/redbus.php?story=resources">Insufficient Resources</a></li>
+			<li><a href="/redbus.php?story=founder">The Founder</a></li>
+			<li><a href="/redbus.php?story=chemistry">The Chemistry</a></li>
+			<li><a href="/redbus.php?story=maintenance">Maintenance and Repair</a></li>
+			<li><a href="/redbus.php?story=listening">Listening to New Voices</a></li>
+			<li><a href="/redbus.php?story=checkup">The Check Up</a></li>
 		</ul>
 		<br/><br/>
-		<div class="sidebarnotes">	
+		<div class="sidebarnotes">
 			<div class="sidebarNoteBig">
 				<p>We have developed a series of vignettes that focuses on important lessons about governance. Each vignette uses a bus/driver/passenger metaphor to illustrate specific aspects of the subject. Watch for new stories. <em>It's fun. It's free. It's educational. Welcome aboard!</em></p>
 			</div>
 		</div>
-		
+
 <?php
 
 //	display Little Red Bus image and sidebar note
