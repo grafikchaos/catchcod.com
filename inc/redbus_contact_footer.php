e32c89158ae17a7b7b97ad80f7a0ad615854c0a1
Josh Johnson <josh@grafikchaos.com>
2012-09-07 15:56:10 -0500

    redbus stories contact block

---
+++ inc/redbus_contact_footer.php
@@ -0,0 +1,7 @@
+<div class='hero-unit redbus'>
+	<p>
+		<strong>Need help with board development?</strong>
+		<br />
+		Contact Jim Storm at <a href="mailto:jstormcod1@aolcom">jstormcod1@aolcom</a> or 612-379-3817.
+	</p>
+</div>
