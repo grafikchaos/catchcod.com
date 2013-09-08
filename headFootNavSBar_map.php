2b8f41b05709ecfc36d91f3c85aec7683f801be6
Josh Johnson <josh@grafikchaos.com>
2013-09-08 16:26:23 -0500

    removes the paragraphs for the under construction sidebar block

--- headFootNavSBar_map.php
+++ headFootNavSBar_map.php
@@ -116,17 +116,7 @@ function displayBook_SB() {
 		        <p>
 		            <em>A Boy From Nisswa</em>
 		            <br /><br />
-		            <a href="<?php echo getBaseUrl(); ?>a-boy-from-nisswa-short-sample.php">A Short Sample</a>
-		            <br /><br />
-		            The construction phase of <em>A Boy From Nisswa</em> has been delayed by the rainy non-constructive
-		           	days of life: the death of my wife last November. Now, however, the sun has begun to shine once
-		           	more and construction has resumed.
-		           	<br /><br />
-		           	Stories like the sixth grade class trip (see attached summary), the realization that
-		           	&quot;I am in the Army now,&quot; and my role as a &quot;charity scout&quot; working with
-		           	Virginia McKnight Binger, are emerging.
-		           	<br /><br />
-		           	Stay tuned and check in as you wish.
+		            <a href="<?php echo getBaseUrl(); ?>a-boy-from-nisswa-short-sample.php">Short Samples</a>
 		        </p>
 		    </div>
 		</div>
