03eaf937beaa08058bd88e2750bc9cc3943e55a2
Josh Johnson <josh@grafikchaos.com>
2013-09-10 21:32:36 -0500

    add another excerpt from 'A Boy from Nisswa'

--- a-boy-from-nisswa-short-sample.php
+++ a-boy-from-nisswa-short-sample.php
@@ -1,72 +1,113 @@
 <?php
-	/**
-	 * comments.php
-	 */
-	include("headFootNavSBar.php");
-	displayHead();
+    /**
+     * comments.php
+     */
+    include("headFootNavSBar.php");
+    displayHead();
 ?>
 
 <div id="mainContent">
-	<div id="content">
-		<h1>An excerpt from <em>A Boy From Nisswa</em></h1>
+    <div id="content">
+        <h1>An excerpt from <em>A Boy From Nisswa</em></h1>
 
-		<blockquote class='excerpt'>
-			<h1 class="title">Sixth Grade Class Trip</h1>
+        <blockquote class='excerpt'>
+            <h1 class="title">Sixth Grade Class Trip</h1>
 
-			<p>
-				Spring 1953 – my sixth grade class trip. The destinations of class trips vary greatly. Some are
-				to mind-expanding places like Washington, D.C. while others are designed more like service projects
-				designed to address some existing need.
-			</p>
+            <p>
+                Spring 1953 – my sixth grade class trip. The destinations of class trips vary greatly. Some are
+                to mind-expanding places like Washington, D.C. while others are designed more like service projects
+                designed to address some existing need.
+            </p>
 
-			<p>
-				In my case, my class trip was a one-day adventure from small-town Nisswa to Minneapolis and St. Paul.
-				It was like an extended field trip.
-			</p>
+            <p>
+                In my case, my class trip was a one-day adventure from small-town Nisswa to Minneapolis and St. Paul.
+                It was like an extended field trip.
+            </p>
 
-			<p>
-				The trip began early in the morning as we boarded the yellow school bus for a journey that would
-				take us to the Armour meatpacking plant, lunch with the governor and on to the Zinsmaster baking
-				plant in south Minneapolis. Perhaps the trip could be described as a hot dog on a bun with the
-				governor.
-			</p>
+            <p>
+                The trip began early in the morning as we boarded the yellow school bus for a journey that would
+                take us to the Armour meatpacking plant, lunch with the governor and on to the Zinsmaster baking
+                plant in south Minneapolis. Perhaps the trip could be described as a hot dog on a bun with the
+                governor.
+            </p>
 
-			<p>
-				First stop: the Armour meatpacking plant in South St. Paul. I came to the realization that this
-				stop must have been planned by a vegetarian among us. How else to explain an experience where live
-				animals become meat products? Yes, we saw, heard and smelled the entire process.
-			</p>
+            <p>
+                First stop: the Armour meatpacking plant in South St. Paul. I came to the realization that this
+                stop must have been planned by a vegetarian among us. How else to explain an experience where live
+                animals become meat products? Yes, we saw, heard and smelled the entire process.
+            </p>
 
-			<p>
-				On to lunch with the governor, probably thought of as a &quot;big deal.&quot; However, in this case,
-				the governor, C. Elmer Anderson, was our neighbor in Nisswa and we didn’t need to travel all the way
-				to St. Paul to have lunch with him.
-			</p>
+            <p>
+                On to lunch with the governor, probably thought of as a &quot;big deal.&quot; However, in this case,
+                the governor, C. Elmer Anderson, was our neighbor in Nisswa and we didn't need to travel all the way
+                to St. Paul to have lunch with him.
+            </p>
 
-			<p>
-				Next and final stop: the Zinsmaster baking plant in south Minneapolis. Having adequately regained our
-				senses and color following the experience at the Armour plant, we experienced wave upon wave of the
-				sweet and mouth-watering smells of dough becoming a variety of delicate and delicious bakery products.
-			</p>
+            <p>
+                Next and final stop: the Zinsmaster baking plant in south Minneapolis. Having adequately regained our
+                senses and color following the experience at the Armour plant, we experienced wave upon wave of the
+                sweet and mouth-watering smells of dough becoming a variety of delicate and delicious bakery products.
+            </p>
 
-			<p>
-				Time to return home. A snack was served on the bus as we headed north &mdash; a bologna sandwich.
-				However, please skip the bologna.
-			</p>
-		</blockquote>
-	</div>	<!-- END CONTENT -->
-</div>	<!-- END MAIN CONTENT -->
+            <p>
+                Time to return home. A snack was served on the bus as we headed north &mdash; a bologna sandwich.
+                However, please skip the bologna.
+            </p>
+        </blockquote>
+
+        <hr />
+
+        <blockquote class='excerpt'>
+            <p>
+                January 1949: A blustery cold winter's night on the north shore of Round Lake, seven miles from the nearest community of Nisswa, Minnesota.  Bare tree limbs, a landscape covered by a foot of snow and a  temperature of 6 degrees F. attest to the reality that this is the dead of winter. Much earlier what sun  had existed had dipped into the far southern horizon. Most residents have long ago migrated south with the only reminder of current habitation being an occasional light flickering in the far-off distance.
+            </p>
+
+            <p>
+                From the outside it is impossible to know just what is taking place within any one of the currently- inhabited homes. In one of these homes, in the tuck-under garage of the home on the north shore of frozen Round Lake, a high school basketball game is about to take place. How can this be possible?  Upon entering that garage, one realizes that this is, in fact, true. A game is about to take place between New York High School (must be a really large school ) and B B Balone (as in sausage) High School. Yes, Hormel hot dogs are being sold at the concession stand.
+            </p>
+
+            <p>
+                The excitement level is high; however, playing conditions are quite precarious. The game time temperature is 48 degrees, thanks to the presence of one portable electric heater. A puddle of half-thawed slush deposited by the car usually parked in this space covers a good portion of the concrete playing surface and a single 60-watt bulb provides some light and a lot of shadows. The floor itself is also very unique. There is a single basket approximately six feet from the floor and the total length of the floor is eighteen feet.  Along the side of the floor sit the usual items found in a garage: rakes, shovels, a lawn mower and a variety of paint cans. Fortunately ,no fire inspectors have shown up.
+            </p>
+
+            <p>
+                How is it that this game is about to take place in this setting? Only one answer: Jimmie Storm has a very rich imagination.
+            </p>
+
+            <p>
+                Time to check in on the action. The pep bands are playing the school fight songs. New York High School shares the same song with Brainerd, Minnesota's high school. The cheerleaders have succeeded in raising the level of excitement to a fever pitch and the radio announcer (no television yet at this location), after a brief commercial for Bob's Auto Mart in Brainerd, has adjusted his earphones and is ready to provide the play-by-play account of the game over the local airways. B B Balone players in their traditional orange and black uniforms are on the floor warming up. Now coming on the floor in their blue and white uniforms are the lads from New York High School.
+            </p>
+
+            <p>
+                Time for player introductions. New York High School: center: John Leonard. He and Jimmie Storm look like identical twins. However, the same can be said about all others assembled as well. Guards:   Squash Leonard, John's younger brother, and Dick Kazmaier. The same Dick Kazmaier who was a football star at Princeton University? If so, has anyone checked his eligibility? (Due to his very recent death, we will never know the answer to this one.)  Two forwards round out the starting five: Dick Haystack and Dick Pigeon. This was before the day of point guards.
+            </p>
+
+            <p>
+                Due to multitasking overload, the starting five from B B Balone are not individually introduced.
+            </p>
+
+            <p>
+                The game itself is a fierce battle between these two rivals. However, in the end it is New York High School that pulls out the victory. One who follows this team understands that this is almost always the case.
+            </p>
+
+            <p>
+                Details of the game will be written the following day by the sportswriter. Yes, you guessed it. The sportswriter will be Jimmie Storm sitting at his desk at Nisswa Elementary School. It will also be noted that the next home game for New York High School will be against the purple and white of Mediterranean High School. Must be a really long road trip for them.
+            </p>
+        </blockquote>
+
+    </div>  <!-- END CONTENT -->
+</div>  <!-- END MAIN CONTENT -->
 
 <div id="sidebar">
-	<?php
-		//	display Little Red Bus image and sidebar note
-		displayBus_SB();
+    <?php
+        //  display Little Red Bus image and sidebar note
+        displayBus_SB();
 
-		//	display Master of Creative Philanthropy Book and sidebar note
-		displayBook_SB();
-	?>
-</div>	<!-- END SIDEBAR -->
+        //  display Master of Creative Philanthropy Book and sidebar note
+        displayBook_SB();
+    ?>
+</div>  <!-- END SIDEBAR -->
 <?php
-	//	display footer
-	displayFooter();
+    //  display footer
+    displayFooter();
 ?>
