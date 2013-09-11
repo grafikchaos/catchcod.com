2b9c4d877f2d36dbb01ab053768eb921f131f697
Josh Johnson <josh@grafikchaos.com>
2013-09-10 21:11:50 -0500

    Adds Jim's key points

--- executive-leadership-key-points.php
+++ executive-leadership-key-points.php
@@ -5,10 +5,30 @@
 
 <div id="mainContent">
     <div id="content">
-        <h1>Executive Directory Leadership Key Points</h1>
+        <h1>Thoughts On Executive Leadership &mdash; Jim Storm</h1>
         <ul class="bodyBullets">
-            <li>Point 1</li>
-            <li>Point 2</li>
+            <li>Leadership is about "we" not "me."</li>
+            <li>Leadership is grounded in your "being" and your "doing."</li>
+            <li>Ask good questions and listen carefully to what is said and not said.</li>
+            <li>Act on your intuitions and not your impulses.</li>
+            <li>Acknowledge and thank others as members of the team.</li>
+            <li>Avoid tunnel vision and get out of your comfort zone.</li>
+            <li>Show me a strong board and I will show you an executive who is a good teacher.</li>
+            <li>Know your weaknesses and find others with these strengths.</li>
+            <li>Be careful what you commit to writing.</li>
+            <li>Maintain a balance in your life.</li>
+            <li>Develop strong oral and written communication skills.</li>
+            <li>Be open to changing your mind as new information is gathered.</li>
+            <li>Work to end relationships on a positive note.</li>
+            <li>Maintain a safe source to which you can turn when troubled.</li>
+            <li>Know when to move on.</li>
+            <li>Don't criticize those who have gone before you or follow you.</li>
+            <li>Personnel issues will never go away. </li>
+            <li>Staff members will grow but seldom change.</li>
+            <li>Fund development is based on relationships cultivated over time.</li>
+            <li>There is very seldom a quick fix for funding problems.</li>
+            <li>Boards raise money when there is a sense of ownership.</li>
+            <li>The mission is that of your organization, not your ego.</li>
         </ul>
     </div>  <!-- END CONTENT -->
 </div>  <!-- END MAIN CONTENT -->
@@ -26,3 +46,4 @@
     //  display footer
     displayFooter();
 ?>
+
